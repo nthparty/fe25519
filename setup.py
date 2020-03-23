4c2197fdbde25d406902df2de4306227ac4d4815
Andrei Lapets <a@lapets.io>
2020-03-22 21:01:59 -0400

    Update version number for current release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="fe25519",
-    version="0.0.0.1",
+    version="0.0.0.2",
     packages=["fe25519",],
     install_requires=[],
     license="MIT",
