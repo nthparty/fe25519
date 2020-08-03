cabd24be26081fca1819557168c36772a7e47337
Andrei Lapets <a@lapets.io>
2020-08-02 22:21:08 -0400

    Update package version for release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="fe25519",
-    version="0.1.1",
+    version="0.1.2",
     packages=["fe25519",],
     install_requires=["parts>=0.2.1","bitlist>=0.3.1","fountains>=0.2.1",],
     license="MIT",
