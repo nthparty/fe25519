24ea81eebccf780a9ceaf630afb170aeb247ee5c
Andrei Lapets <a@lapets.io>
2021-09-25 17:00:32 -0400

    Add config, template, and build files for auto-generating docs; update README.

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
+# The lines below are parsed by `docs/conf.py`.
+name = "fe25519"
+version = "0.2.0"
+
 setup(
-    name="fe25519",
-    version="0.2.0",
+    name=name,
+    version=version,
     packages=["fe25519",],
     install_requires=["parts>=0.2.1","bitlist>=0.3.1","fountains>=0.2.1",],
     license="MIT",
